ac93fe0c3e6e7699e391f70d8b82e5daac0aed6c
miracoly <68049792+miracoly@users.noreply.github.com>
2024-06-24 17:34:08 +0200

    feat(server): add my server solution

--- server/src/resolvers.ts
+++ server/src/resolvers.ts
@@ -1,28 +1,48 @@
-import { Resolvers } from "./types";
+import {Resolvers} from "./types";
+import {GraphQLError} from 'graphql/error';
 
 export const resolvers: Resolvers = {
   Query: {
     // returns an array of Tracks that will be used to populate the homepage grid of our web client
-    tracksForHome: (_, __, { dataSources }) => {
+    tracksForHome: (_, __, {dataSources}) => {
       return dataSources.trackAPI.getTracksForHome();
     },
 
     // get a single track by ID, for the track page
-    track: (_, { id }, { dataSources }) => {
+    track: (_, {id}, {dataSources}) => {
       return dataSources.trackAPI.getTrack(id);
     },
 
     // get a single module by ID, for the module detail page
-    module: (_, { id }, { dataSources }) => {
+    module: (_, {id}, {dataSources}) => {
       return dataSources.trackAPI.getModule(id);
     },
   },
+  Mutation: {
+    incrementTrackViews: async (_, {id}, {dataSources}) =>{
+      try {
+        const track = await dataSources.trackAPI.incrementTrackViews(id);
+        return {
+          code: 200,
+          success: true,
+          message: `Successfully incremented number of views for track ${id}`,
+          track
+        };
+      } catch (err) {
+        return {
+          code: err.extensions?.response?.status,
+          success: false,
+          message: err.extensions?.response?.body
+        }
+      }
+    }
+  },
   Track: {
-    author: ({ authorId }, _, { dataSources }) => {
+    author: ({authorId}, _, {dataSources}) => {
       return dataSources.trackAPI.getAuthor(authorId);
     },
 
-    modules: ({ id }, _, { dataSources }) => {
+    modules: ({id}, _, {dataSources}) => {
       return dataSources.trackAPI.getTrackModules(id);
     },
   },
